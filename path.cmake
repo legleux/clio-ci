fdb637794835bb962de897e5db188dec0d0412d8
Michael Legleux <mlegleux@ripple.com>
2022-09-17 14:20:13 -0700

    Test on CentOS and build rpm packages

---
+++ path.cmake
@@ -0,0 +1,6 @@
+set(CLIO_ROOT  ../clio)
+message("CLIO_ROOT: ${CLIO_ROOT}")
+# get_filename_component(${CLIO_ROOT} CLIO_ROOT ABSOLUTE)
+get_filename_component(CLIO_ROOT ${CLIO_ROOT} ABSOLUTE)
+
+message("CLIO_ROOT: ${CLIO_ROOT}")
