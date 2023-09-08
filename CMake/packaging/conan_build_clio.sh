1b4cc75b2a66163a51fc6d43dc48fbc116057aad
Michael Legleux <mlegleux@ripple.com>
2023-09-07 17:45:14 -0700

    dir

--- CMake/packaging/conan_build_clio.sh
+++ CMake/packaging/conan_build_clio.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-SRC_DIR=/clio/clio
+SRC_DIR=clio
 BUILD_DIR=${SRC_DIR}/build
 BUILD_CONFIG=Release
 TESTS=False
@@ -13,7 +13,7 @@ PACKAGING=True
 
 #conan export external/snappy snappy/1.1.10@
 echo "I am in $PWD"
-pushd /clio && git config --global safe.directory '*' && popd
+pushd ${SRC_DIR} && git config --global safe.directory '*' && popd
 
 # TODO: check if remote exists before adding
 conan remote add --insert 0 conan-non-prod http://18.143.149.228:8081/artifactory/api/conan/conan-non-prod || true
